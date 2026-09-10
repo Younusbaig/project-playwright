a890b3473b403fa01aa3fa151fe9c5e00739cf46
Muhammad Younus Baig <younus@Muhammads-MacBook-Pro.local>
2026-09-10 16:26:16 +0500

    fix test assertions

--- tests/auth/signup.spec.js
+++ tests/auth/signup.spec.js
@@ -11,6 +11,7 @@ test("Successfully created account", async ({page}) => {
     await signUpPage.goto();
     await page.waitForLoadState('networkidle');
     await signUpPage.createAccount(users.validUser.email, users.validUser.password);
+    await page.waitForLoadState('networkidle');
     await expect(page).toHaveURL("https://test-ecommerce-store--muhammadbaig199.replit.app/");
 })
 
